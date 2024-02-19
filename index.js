c14e8fa73525e2586ffe85388314a5d253289ecc
sheraz <sherazkarim12@gmail.com>
2024-02-19 18:27:59 +0500

    third commit

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+document.querySelectorAll("button")[0].addEventListener("click", function(){
+    alert ("working!!")
+})
 
-document.querySelector("button").addEventListener("click", handleClick);
+for (let i =0; i<document.querySelectorAll(".drum").length ; i++){
+alert ("working!")
+}
+for (var i =0; i< )
 
-function handleClick() {
+
+for (var )
+
+for (let i = 0 < document.querySelector(".drum").length ; i++)
+    document.querySelectorAll("button")[0].addEventListener("click", function(){
+
+        alert ("i got clicked");
      
-    alert("I got clicked ");
-} 
 
+)};
